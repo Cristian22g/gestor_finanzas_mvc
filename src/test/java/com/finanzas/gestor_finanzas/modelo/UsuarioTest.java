2292bac85ffa09be73bc4cb1e5d478aa1d4dcfd9
Cris <cristiangraciaacosta@cdpjosecabrera.es>
2025-05-16 10:34:34 +0200

    Dependencia JUnit y Test creados

---
+++ src/test/java/com/finanzas/gestor_finanzas/modelo/UsuarioTest.java
@@ -0,0 +1,17 @@
+package com.finanzas.gestor_finanzas.modelo;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UsuarioTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
